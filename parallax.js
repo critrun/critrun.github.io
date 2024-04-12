02aef77b6d08d26ab623224da3c37f8cc7c35407
crit <54358989+critrun@users.noreply.github.com>
2024-04-12 17:50:44 +0200

    Style refinement.

--- parallax.js
+++ parallax.js
@@ -7,8 +7,8 @@
     function parallax(e) {
         let _w = window.innerWidth/2;
         let _h = window.innerHeight/2;
-        elem.style.backgroundPosition = `${50 - (e.clientX - _w) * -0.01}% ${50 - (e.clientY - _h) * -0.01}%`;
-        elem1.style.left =  `${- (e.clientX - _w) * -0.001}vh`;
+        elem.style.backgroundPosition = `${50 - (e.clientX - _w) * -0.03}% ${50 - (e.clientY - _h) * -0.03}%`;
+        elem1.style.left =  `${- (e.clientX - _w) * -0.001}vw`;
         elem1.style.top =  `${- (e.clientY - _h) * -0.001}vh`;
     }
 
