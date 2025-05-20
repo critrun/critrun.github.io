1ca88cdf4ffc0187a89508e7971c0a095018a4bb
Ronja <54358989+critrun@users.noreply.github.com>
2025-05-20 18:09:34 +0200

    caching bug fix

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,21 @@
 var img_loads = 0;
+var domLoaded = false;
+
+addEventListener("DOMContentLoaded", (event) => {domLoaded=true;});
 
 function upgradeImage(object, hq_src) {
-    //object.style.backgroundImage = "url(" + lq_src + ")";
+
+    if (domLoaded) {
+        _upgradeImage(object, hq_src);
+    }
+    else {
+        addEventListener("DOMContentLoaded", (event) => {
+        _upgradeImage(object, hq_src);
+    });
+    }
+}
+
+function _upgradeImage(object, hq_src) {
     var all_imgs = document.getElementsByClassName("upgradeImage");
     object.parentNode.style.backgroundImage = "url(" + object.src + ")";
     object.innerHTML = hq_src;
