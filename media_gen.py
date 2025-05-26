7dad0ede201e9d264838d38737cc1b3bde6c90de
Ronja <54358989+critrun@users.noreply.github.com>
2025-05-26 21:31:23 +0200

    Fully automated lq generation

--- media_gen.py
+++ media_gen.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, remove
 from os.path import isfile, join
 import cv2
 
@@ -6,10 +6,15 @@ mediapaths = ["portfolio/assets/3d", "portfolio/assets/photography"]
 MAX_THUMB_RES = 480
 
 for mediapath in mediapaths:
+    # Delete old low-quality
+    lqfiles = [join(join(mediapath, "lq"), f) for f in listdir(join(mediapath, "lq")) if isfile(join(join(mediapath, "lq"), f)) and not ".json" in f]
+    for lqfile in lqfiles:
+        remove(lqfile)
+    
+    # Generate new low-quality
     files = [f for f in listdir(mediapath) if isfile(join(mediapath, f)) and not ".json" in f]
     onlyfiles = str([f for f in listdir(mediapath) if isfile(join(mediapath, f)) and not ".json" in f]).replace(" ","").replace("[", "[\n    ").replace(",",",\n    ").replace("]","\n]").replace("'", '"')
     open(f"{mediapath}/media.json", "w+", encoding="utf-8").write(onlyfiles)
-    print(files)
     for file in files:
         try:
             im = cv2.imread(mediapath+"/"+file)
